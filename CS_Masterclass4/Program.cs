6df469477e33a8483d4ba962510e3459836b1d3b
gborie <gborie7@gmail.com>
2018-07-04 18:41:01 +0100

    if, else if, else  + TryParse

--- CS_Masterclass4/Program.cs
+++ CS_Masterclass4/Program.cs
@@ -12,22 +12,42 @@ namespace CS_Masterclass4
         {
             Console.WriteLine("Please enter the temperature: ");
             string input = Console.ReadLine();
-            int temperature = int.Parse(input);
 
-            if(temperature < 18)
+            int temperature;
+            int output;
+
+            if (int.TryParse(input, out output))
+            {
+                temperature = output;
+            }
+            else
+            {
+                temperature = 0;
+                Console.WriteLine("Value entered not a number. 0 set as temperature");
+            }
+
+
+            if (temperature < 18)
             {
                 Console.WriteLine("Take a coat");         
             }
 
-            if(temperature == 18)
+            else if(temperature == 18)
             {
                 Console.WriteLine("Trousers and hoodie!");
             }
 
-            if(temperature > 18)
+            else if(temperature > 18 && temperature < 30)
             {
                 Console.WriteLine("Shorts weather today!");
             }
+
+            else 
+            {
+                Console.WriteLine("Naked day!");
+            }
+
+
 
             Console.Read();
         }
