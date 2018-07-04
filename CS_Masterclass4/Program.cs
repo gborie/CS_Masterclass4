2a50192bfe5fd7a14913b062506acb1393d1dccf
gborie <gborie7@gmail.com>
2018-07-04 17:56:07 +0100

    Intro to Decision Making - if

--- CS_Masterclass4/Program.cs
+++ CS_Masterclass4/Program.cs
@@ -10,6 +10,24 @@ namespace CS_Masterclass4
     {
         static void Main(string[] args)
         {
+            int temperature = 25;
+
+            if(temperature < 10)
+            {
+                Console.WriteLine("Take a coat");         
+            }
+
+            if(temperature == 10)
+            {
+                Console.WriteLine("It's 10 degrees!");
+            }
+
+            if(temperature > 10)
+            {
+                Console.WriteLine("Cozy warm!");
+            }
+
+            Console.Read();
         }
     }
 }
