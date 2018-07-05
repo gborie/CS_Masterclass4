d204df25f96afe03a9c89d7f1acae916d8eb6782
gborie <gborie7@gmail.com>
2018-07-05 10:17:45 +0100

    Switch Statement

--- CS_Masterclass4/Program.cs
+++ CS_Masterclass4/Program.cs
@@ -9,51 +9,24 @@ namespace CS_Masterclass4
     class Program
     {
 
-        // Global variables used in multiple methods
-        static string username;
-        static string password;
 
         static void Main(string[] args)
         {
-            Register();
-            Login();
+            int age = 19;
+            switch (age)
+            {
+                case 15:
+                    Console.WriteLine("Too young to party in the club!");
+                    break;
+                case 25:
+                    Console.WriteLine("Good to go!");
+                    break;
+                default:
+                    Console.WriteLine("How old are you?");
+                    break;
+            }
             Console.Read();
         }
 
-        //Method to Register user with username and password
-        public static void Register()
-        {
-            Console.WriteLine("Please enter your username");
-            username = Console.ReadLine();
-            Console.WriteLine("Please enter your password");
-            password = Console.ReadLine();
-            Console.WriteLine("Registration completed");
-            Console.WriteLine("--------------------------------------------");
-        }
-
-        //Method to login by checking if username and password are matching info registered
-        public static void Login()
-        {
-            Console.WriteLine("LOGIN PAGE: Please enter your username");
-            string user = Console.ReadLine();
-            if(user == username)
-            {
-                Console.WriteLine("Please enter your password");
-                string pass = Console.ReadLine();
-                if(pass == password)
-                {
-                    Console.WriteLine("Congratulations, you are now logged in!");
-                }
-                else
-                {
-                    Console.WriteLine("Login failed, wrong password. Restart program");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Login failed, wrong username. Restart program");
-            }
-            
-        }
     }
 }
