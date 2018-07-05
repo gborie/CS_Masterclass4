94125ec27025bb0cf6b6a9a07481ace98b7715cf
gborie <gborie7@gmail.com>
2018-07-05 10:13:00 +0100

    Challenge - if statements

--- CS_Masterclass4/Program.cs
+++ CS_Masterclass4/Program.cs
@@ -8,29 +8,52 @@ namespace CS_Masterclass4
 {
     class Program
     {
+
+        // Global variables used in multiple methods
+        static string username;
+        static string password;
+
         static void Main(string[] args)
         {
-            bool isAdmin = false;
-            bool isRegistered = true;
-            string userName = "";
-            Console.WriteLine("Please enter your usename");
-            userName = Console.ReadLine();
-            
-            if(isRegistered)
-            {
-                Console.WriteLine("Hi there, registered user");
-                if(userName != "")
-                {
-                    Console.WriteLine("Hi there, " + userName);
-                    if (userName.Equals("Admin"))
-                    {
-                        Console.WriteLine("Hi there, Admin");
-                    }
-                }
-
-            }
+            Register();
+            Login();
             Console.Read();
+        }
 
+        //Method to Register user with username and password
+        public static void Register()
+        {
+            Console.WriteLine("Please enter your username");
+            username = Console.ReadLine();
+            Console.WriteLine("Please enter your password");
+            password = Console.ReadLine();
+            Console.WriteLine("Registration completed");
+            Console.WriteLine("--------------------------------------------");
+        }
+
+        //Method to login by checking if username and password are matching info registered
+        public static void Login()
+        {
+            Console.WriteLine("LOGIN PAGE: Please enter your username");
+            string user = Console.ReadLine();
+            if(user == username)
+            {
+                Console.WriteLine("Please enter your password");
+                string pass = Console.ReadLine();
+                if(pass == password)
+                {
+                    Console.WriteLine("Congratulations, you are now logged in!");
+                }
+                else
+                {
+                    Console.WriteLine("Login failed, wrong password. Restart program");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Login failed, wrong username. Restart program");
+            }
+            
         }
     }
 }
